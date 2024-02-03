2d6d97d7c58cda8137992d2041ffe7341321ad5e
Gmafo <158758589+Gmafo@users.noreply.github.com>
2024-02-03 19:00:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 20
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p1',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}.si quieres suguiereme un juego para hacer :). Juego hecho por Gonchi.Mafo`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p1','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p1','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    console.log(numeroGenerado)
+    console.log(listaNumerosSorteados)
+    if (listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p',`ya se sortearon todos los numeros posibles del 1 al ${numeroMaximo}`);
+    }else {
+
+
+if (listaNumerosSorteados.includes(numeroGenerado)) {
+    return generarNumeroSecreto(); 
+}else{
+    listaNumerosSorteados.push(numeroGenerado)
+    return numeroGenerado;
+        } 
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento("h1","El juego del numero secreto");
+    asignarTextoElemento("p",`coloca un numero del 1 al ${numeroMaximo} (los numeros no se repiten)`);
+    asignarTextoElemento('p1','')
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+condicionesIniciales();
